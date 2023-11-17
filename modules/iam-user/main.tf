e9c0ecdae953a43d33661fd2c750639e75811027
Malshan Peiris <malshan@codegen.net>
2023-11-17 14:52:50 +0530

    bugfix in for_each

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -4,7 +4,7 @@
 # User
 ########################
 resource "oci_identity_user" "this" {
-  for_each       = var.users
+  for_each       = [ for user in var.users: user.name ] 
   compartment_id = var.tenancy_ocid
   name           = var.users[each.key].name
   description    = var.users[each.key].description
